4fd94fae3cc2d86d6861ece71864738405d77c23
GitHub Action <action@github.com>
2023-08-16 23:07:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/idevelopthings/customlanguageplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/idevelopthings/customlanguageplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.idevelopthings.customlanguageplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.idevelopthings.customlanguageplugin.MyBundle
+import com.github.idevelopthings.customlanguageplugin.services.MyProjectService
 import javax.swing.JButton
 
 
